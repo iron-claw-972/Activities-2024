a0594aabd640fc328c8a4da4662402a8f167d3b9
Kyle-Eldridge <eldk6798@lgsstudent.org>
2024-07-13 14:25:55 -0700

    New file for activity 4

---
+++ src/main/java/frc/robot/util/ShuffleBoard/Tabs/SubsystemTab.java
@@ -0,0 +1,25 @@
+
+package frc.robot.util.ShuffleBoard.Tabs;
+
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import frc.robot.util.ShuffleBoard.ShuffleBoardTabs;
+
+
+public class SubsystemTab extends ShuffleBoardTabs {
+    // TODO 2.3.11: Create variable for subsystem
+
+    public SubsystemTab(){
+        
+    }
+
+    public void createEntries(){
+        tab = Shuffleboard.getTab("Subsystem");
+
+        // TODO 4.3.1: Add PID
+        
+    }
+
+
+    public void update(){
+    }
+}
