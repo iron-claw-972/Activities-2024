918ea07e478dc7ad7e20bbb44ed6d67289b98dc4
moo <moogoesmeow123@gmail.com>
2024-10-03 15:20:34 -0700

    make work more

--- src/main/java/frc/robot/util/ShuffleBoard/Tabs/AutoTab.java
+++ src/main/java/frc/robot/util/ShuffleBoard/Tabs/AutoTab.java
@@ -1,10 +1,13 @@
 package frc.robot.util.ShuffleBoard.Tabs;
 
+import java.awt.geom.Point2D;
+
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
 import frc.robot.Robot;
+import frc.robot.commands.BangBangDriveCommand;
 import frc.robot.commands.FunnyCommand;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Ting;
@@ -32,6 +35,7 @@ public class AutoTab extends ShuffleBoardTabs {
 
         // TODO 3.3.8: Add your Bang-Bang drive command here
 
+        autoCommand.addOption("bingbongboom", new BangBangDriveCommand(drive, new Point2D.Double(1,1)));
         // TODO 3.3.11: Add your Bang-Bang command for your subsystem here
         
         tab.add(autoCommand);
