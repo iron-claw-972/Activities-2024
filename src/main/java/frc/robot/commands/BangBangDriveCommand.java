a812a07adcc7554eda4fedb92a50dcee2b7d6465
moo <moogoesmeow123@gmail.com>
2024-10-03 15:11:47 -0700

    make work bang bang

--- src/main/java/frc/robot/commands/BangBangDriveCommand.java
+++ src/main/java/frc/robot/commands/BangBangDriveCommand.java
@@ -9,6 +9,7 @@ import java.awt.geom.Point2D;
 public class BangBangDriveCommand extends Command{
     Drivetrain driv;
     Point2D setpoint;
+    int tim;
     public BangBangDriveCommand(Drivetrain drev, Point2D setpoint){
         driv = drev;
         this.setpoint = setpoint;
@@ -17,21 +18,56 @@ public class BangBangDriveCommand extends Command{
     @Override
     public void initialize(){
         driv.resetEncoders();
+        tim = 0;
     }
 
     @Override
     public void execute(){
-        
-
-    }
-
-    private double calculateTrajectory(){
-        double angle = Math.atan2(setpoint.getY(), setpoint.getX());
-        if ((setpoint.getY() > 1) && (setpoint.getX() > 1){
+        tim ++;
+        if (tim <= 250){
+            driv.arcadeDrive(0, normalize(calculateTurnAngle(setpoint, (driv.getGyroAngle().getDegrees())), -180, 180));
+        }else{
+            driv.arcadeDrive(1, normalize(calculateTurnAngle(setpoint, (driv.getGyroAngle().getDegrees())), -180, 180));
 
         }
-        
+
     }
+    @Override
+    public void end(boolean interrupted){
+        driv.arcadeDrive(0, 0);
+    }
+
+    public static double calculateTurnAngle(Point2D target, double startingAngle) {
+        double targetAngle = Math.toDegrees(Math.atan2(target.getY(), target.getX()));
+        
+        double turnAngle = targetAngle - startingAngle;
+
+        // normalize
+        while (turnAngle > 180) {
+            turnAngle -= 360;
+        }
+        while (turnAngle < -180) {
+            turnAngle += 360;
+        }
+
+        return turnAngle;
+    }
+    
+    @Override
+    public boolean isFinished(){
+        if (getDistance() < .5){
+            return true;
+        }
+        return false;
+    }
+
+    public static double normalize(double value, double min, double max) {
+        
+        double normalized = (value - min) / (max - min);
+        
+        return normalized * 2 - 1;
+    }
+
 
     private double getDistance(){
         return setpoint.distance(0, 0);
