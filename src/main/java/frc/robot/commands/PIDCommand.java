989b00ce4d799681a31ea6e7a7af0b01a2942f6c
iefomit <108955303+iefomit@users.noreply.github.com>
2024-11-13 17:19:58 -0800

    Added PID Command

---
+++ src/main/java/frc/robot/commands/PIDCommand.java
@@ -0,0 +1,28 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.DriveSub;
+
+public class PIDCommand extends Command {
+    private final DriveSub driveSub;
+    private final double setpoint;
+
+    // constructor
+    public PIDCommand(DriveSub driveSub, double setpoint) {
+        this.driveSub = driveSub;
+        this.setpoint = setpoint;
+        addRequirements(driveSub);
+    }
+
+    // called once when command initially scheduled
+    @Override
+    public void initialize() {
+        driveSub.spinTo(setpoint); // set the PID setpoint
+    }
+
+    // called once per scheduler run to check if the command is finished
+    @Override
+    public boolean isFinished() {
+        return driveSub.atSetpoint(); // checks if the subsystem has reached the target
+    }
+}
