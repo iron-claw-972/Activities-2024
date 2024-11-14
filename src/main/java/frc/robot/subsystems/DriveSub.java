c48a25f5e3e058aad177cb3dd0d8ad29ea71ab63
iefomit <108955303+iefomit@users.noreply.github.com>
2024-11-13 17:08:25 -0800

    Finished 5.1

--- src/main/java/frc/robot/subsystems/DriveSub.java
+++ src/main/java/frc/robot/subsystems/DriveSub.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix6.hardware.TalonFX;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
@@ -16,7 +17,8 @@ public class DriveSub extends SubsystemBase {
     private SingleJointedArmSim armSim;
     private Mechanism2d mech2d;
     private MechanismLigament2d wheelLigament;
-    
+    private PIDController pidController;
+
     // Constructor
     public DriveSub(int motorID) {
 
@@ -32,15 +34,19 @@ public class DriveSub extends SubsystemBase {
             Double.POSITIVE_INFINITY, // max angle 
             false,      // sim gravity false
             0);   // disable gravity
-
         }
+
         // initialize mechanism2d and ligament
         mech2d = new Mechanism2d(100, 100);
         wheelLigament = new MechanismLigament2d("Wheel", 30, 0);
 
+        //intialize pid
+        pidController = new PIDController(0.1, 0, 0);
+        pidController.setTolerance(0.05);
+
+
         // add the wheel ligament to the Mechanism2d
         mech2d.getRoot("pivot", 50, 50).append(wheelLigament);
-
     }
 
     // method to get the Mechanism2d object
@@ -49,11 +55,12 @@ public class DriveSub extends SubsystemBase {
     }
 
     // method motor speed
-    public void setSpeed(double speed) {
+    public void setSpeed(double targetPosition) {
         if (RobotBase.isReal()) {
-            motor.set(speed);
+            motor.set(targetPosition);
         } else {
-            armSim.setInput(Constants.ROBOT_VOLTAGE * speed); // multiply robot voltage by speed
+            double output = pidController.calculate(getPosition(), targetPosition);
+            armSim.setInput(Constants.ROBOT_VOLTAGE * output);
         }
     }
 
@@ -73,6 +80,21 @@ public class DriveSub extends SubsystemBase {
         }
     }
 
+    // method to set PID setpoint
+    public void spinTo(double targetPosition) {
+        pidController.reset();
+        pidController.setSetpoint(targetPosition);
+    }
+
+        // method to check if motor is at setpoint
+        public boolean atSetpoint() {
+            return pidController.atSetpoint();
+        }
+    
+        // method to get PID controller
+        public PIDController getPID() {
+            return pidController;
+        }
 
     // method interia
     private double getMomentOfInertia() {
@@ -89,16 +111,17 @@ public class DriveSub extends SubsystemBase {
     public void periodic() {
 
         if (!RobotBase.isReal()) { 
+            // get the PID output and use it to set motor speed toward setpoint
+            double output = pidController.calculate(getPosition());
+            setSpeed(output);
 
-            // get simulated pos. in rotations
+            // get simulated pos. in rotations, convert to degrees
             double rotations = getPosition();
             double degrees = rotations * 360;
-
             wheelLigament.setAngle(degrees);
 
+            // update arm sim
             armSim.update(Constants.LOOP_TIME);
-
         }
     }
 }
-
