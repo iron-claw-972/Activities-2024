6bf5572db85adf0018033846e3f786e1087ff62a
moo <moogoesmeow123@gmail.com>
2024-12-01 16:48:46 -0800

    fdsafdsa

--- src/main/java/frc/robot/controls/GameControllerDriverConfig.java
+++ src/main/java/frc/robot/controls/GameControllerDriverConfig.java
@@ -1,8 +1,17 @@
 package frc.robot.controls;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import frc.robot.commands.ArcadeDriveCommand;
+import frc.robot.commands.BangBangDriveCommand;
 import frc.robot.commands.DoNothing;
+import frc.robot.commands.FunnyCommand;
+import frc.robot.commands.TingBangBang;
+import frc.robot.commands.TurnyCommand;
 import frc.robot.constants.Constants;
 import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Ting;
 import lib.controllers.GameController;
 import lib.controllers.GameController.Axis;
 import lib.controllers.GameController.Button;
@@ -20,13 +29,14 @@ public class GameControllerDriverConfig extends BaseDriverConfig {
   @Override
   public void configureControls() {
     // TODO 4.1.1: Change to your auto command
-    controller.get(Button.A).onTrue(new DoNothing());
+    controller.get(Button.B).onTrue(new FunnyCommand(getDrivetrain(), new Ting(40)));
     // TODO 4.1.3: Add Bang-Bang drive command
-
+    controller.get(Button.A).onTrue(new BangBangDriveCommand(getDrivetrain(), new Pose2d(10,10,new Rotation2d(0))));
     // TODO 4.1.4: Add subsystem Bang-Bangs
-
+    controller.get(Button.X).onTrue(new TingBangBang(new Ting(13), (double) 45)).onFalse(new TingBangBang(new Ting(13), (double) 0));
+    controller.get(Button.Y).whileTrue(new TurnyCommand(1, new Ting(16)));
     // TODO 4.2.2: Make robot spin while a button is pressed
-
+    controller.get(Button.LB).whileTrue(RunCommand());
     // TODO 4.3.1: Add more triggers
   }
 
